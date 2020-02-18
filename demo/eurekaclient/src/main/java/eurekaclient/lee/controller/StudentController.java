1b218c75a681c314972ad73c27fcd6d9d048eeeb
Ctrl08 <390022566@qq.com>
2020-02-18 20:16:22 +0800

    简单测试服务提供者中的业务访问状态

---
+++ demo/eurekaclient/src/main/java/eurekaclient/lee/controller/StudentController.java
@@ -0,0 +1,42 @@
+package eurekaclient.lee.controller;
+
+import eurekaclient.lee.entity.Student;
+import eurekaclient.lee.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository repository;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findById(@PathVariable("id")Integer id){
+        return repository.findById(id);
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Student student){
+        repository.saveOrUpdate(student);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Student student){
+        repository.saveOrUpdate(student);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id")Integer id){
+        repository.deleteById(id);
+    }
+
+}
---
+++ demo/eurekaclient/src/main/java/eurekaclient/lee/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package eurekaclient.lee.repository;
+
+import eurekaclient.lee.entity.Student;
+
+import java.util.Collection;
+
+public interface StudentRepository {
+    public Collection<Student> findAll();
+    public Student findById(Integer id);
+    public void saveOrUpdate(Student student);
+    public void deleteById(Integer id);
+}
