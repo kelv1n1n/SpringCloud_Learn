6809f7728e7effc6cb99eead9a668b6530f8cd85
Ctrl08 <390022566@qq.com>
2020-02-18 19:46:50 +0800

    简单完成服务提供者的注册

---
+++ demo/eurekaclient/src/main/java/eurekaclient/lee/ServerProviderApplication.java
@@ -0,0 +1,11 @@
+package eurekaclient.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerProviderApplication.class,args);
+    }
+}
