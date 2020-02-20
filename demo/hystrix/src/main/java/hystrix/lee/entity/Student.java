f3ab494c974b18821f8d454f8723f3ae2740f3ba
Ctrl08 <390022566@qq.com>
2020-02-20 16:54:27 +0800

    简单实现hystrix对请求的数据监控

---
+++ demo/hystrix/src/main/java/hystrix/lee/entity/Student.java
@@ -0,0 +1,16 @@
+package hystrix.lee.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+}
---
+++ demo/hystrix/src/main/java/hystrix/lee/feign/FeignProviderClient.java
@@ -0,0 +1,17 @@
+package hystrix.lee.feign;
+
+import hystrix.lee.entity.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@FeignClient(value = "serverProvider")
+public interface FeignProviderClient {
+
+    @GetMapping("/student/findAll")
+    public Collection<Student> findAll();
+
+    @GetMapping("/student/index")
+    public String index();
+}
