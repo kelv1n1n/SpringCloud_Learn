228f8a360af248f8b11c69cad9c6175714905f0b
Ctrl08 <390022566@qq.com>
2020-02-20 20:11:33 +0800

    简单实现Spring Cloud Config 远程配置中心

---
+++ demo/githubconfigclient/src/main/java/configclient/lee/GithubConfigClientApplication.java
@@ -0,0 +1,11 @@
+package configclient.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubConfigClientApplication.class,args);
+    }
+}
